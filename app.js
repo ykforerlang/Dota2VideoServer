0f54066b3808eee4be889919d300e1465ecd5f23
ykforerlang <1527997464@qq.com>
2016-08-26 18:43:36 +0800

    添加 video内容

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var league = require('./routes/league')
 var match = require('./routes/match')
+var video = require('./routes/video')
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/league', league)
 app.use('/match', match)
+app.use('/video', video)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/video.js
+++ routes/video.js
@@ -9,7 +9,21 @@ const router = express.Router();
 const {videoDb} = require('../service/db')
 
 router.get("/list", (req, res) => {
-    const {maxId} = req.query
+    const {maxId, minId} = req.query
 
+    const videos = videoDb.get('video')
+        .filter(video => {
+            if (maxId) {
+                return video.videoId < maxId
+            }
+            if (minId) {
+                return video.videoId > minId
+            }
+            return true
+        })
+        .take(50)
+        .value()
+
+    res.json(videos)
 })
 module.exports = router;
